aeee2c6045dd6c81d60bdf95cbc171b8e8f4eb5d
Marco Poelsma <marco.poelsma@gracia.lasalle.cat>
2024-10-24 22:17:59 +0200

    amogus

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println(3);
         System.out.println("Hello world!");
         System.out.println("Jijijjija");
     }
